9bcd38d527f20d5ec9e30ed2a2ba8e0c78fffc91
koi-chan <koi-chan@mail.kazagakure.net>
2020-12-31 21:02:21 +0900

    ユーザーのマイページを作成

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,4 +1,8 @@
 class MypageController < ApplicationController
+  before_action :require_sign_in
+  before_action :authenticate_user!
+
   def index
+    @user = User.find(current_user.id)
   end
 end

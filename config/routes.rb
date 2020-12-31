562542222672f7bb7118cdc5cba09936620f7fa3
koi-chan <koi-chan@mail.kazagakure.net>
2020-12-31 20:10:21 +0900

    rails g controller mypage index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources 'original_tables'
+  resources 'mypage', only: %i(index)
   resources 'chat_system_authentication_tokens', only: %i(index new create destroy)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

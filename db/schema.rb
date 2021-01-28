8bb1736ba352c586778536b74023ae37a6f4415a
koi-chan <koi-chan@mail.kazagakure.net>
2021-01-28 13:26:43 +0900

    rails g migration AddColumnPublicToOriginalTable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_081357) do
+ActiveRecord::Schema.define(version: 2021_01_28_042323) do
 
   create_table "chat_system_authentication_tokens", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_01_27_081357) do
     t.string "definition", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "public", default: false, null: false
     t.index ["user_id"], name: "index_original_tables_on_user_id"
   end
 

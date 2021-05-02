ee80bf2f31ac773a658e0bbc043ef344e8836861
koi-chan <koi-chan@mail.kazagakure.net>
2021-05-02 15:18:36 +0900

    同じアカウントを連携出来ないようにした

--- app/models/chat_system_link.rb
+++ app/models/chat_system_link.rb
@@ -4,6 +4,12 @@ class ChatSystemLink < ApplicationRecord
   belongs_to :user
   belongs_to :chat_system
 
+  validate :validate_uniqueness
+
+  # サーバの名前を返す
+  # チャットボットが起動していたら、チャットシステムから名前を取得する
+  # 起動していなければ、データベースに保存されている ID を返す
+  # return [Integer/String]
   def server
     bot_status_client = Cd2c::ChatBots::StatusClient.new(
       Rails.application.config.bot_status.socket_path,
@@ -21,6 +27,10 @@ class ChatSystemLink < ApplicationRecord
     bot_status || server_id
   end
 
+  # アカウントの名前を返す
+  # チャットボットが起動していたら、チャットシステムから名前を取得する
+  # 起動していなければ、データベースに保存されている ID を返す
+  # return [Integer/String]
   def account
     bot_status_client = Cd2c::ChatBots::StatusClient.new(
       Rails.application.config.bot_status.socket_path,
@@ -38,4 +48,19 @@ class ChatSystemLink < ApplicationRecord
     bot_status || account_id
   end
 
+  private
+
+  # 既に連携済みのチャットシステムのアカウントを重複して連携登録させない
+  # return [Boolean]
+  def validate_uniqueness
+    case chat_system_id
+    when 1
+      # discord
+      # server_id と account_id が両方とも同一の場合、
+      # 同じチャットアカウントと見做す
+      if self.where(server_id: server_id, account_id: account_id).count > 0
+        errors.add(:account_id, '既にこのアカウントは連携されています')
+      end
+    end
+  end
 end
